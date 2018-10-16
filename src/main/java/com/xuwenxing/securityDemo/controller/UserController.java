cb23b6631c04410cd2e8b2ca49a82ed9634a89f2
XWX19 <1073876389@qq.com>
2018-10-16 16:45:30 +0800

    :sparkles: 添加新特性 文件上传 csrf

--- src/main/java/com/xuwenxing/securityDemo/controller/UserController.java
+++ src/main/java/com/xuwenxing/securityDemo/controller/UserController.java
@@ -8,11 +8,17 @@ import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by xuwx on 2018/10/9.
@@ -24,6 +30,13 @@ public class UserController
     @Autowired
     private UserService userService;
 
+    /**
+     * 默认情况下安全配置没有使用指定路径的话登录成功后会登录到当前路径下
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     */
     @RequestMapping("/")
     public String allUser(HttpServletRequest request, HttpServletResponse response, Model model){
         SecurityContextImpl securityContextImpl = (SecurityContextImpl) request
@@ -45,6 +58,10 @@ public class UserController
         return "showAllUser";
     }
 
+    @RequestMapping("/uploadFiles")
+     public String uploadFiles(){
+        return "uploadFiles";
+    }
 
 
     @RequestMapping("/userLogin")
@@ -61,6 +78,38 @@ public class UserController
             return "/login";
         }
     }
+
+    /**
+     * 文件上传 文件处理类
+     */
+    @RequestMapping("/myUpload")
+    @ResponseBody
+     public String myUpload(@RequestParam("myUploadFile")MultipartFile file){
+        System.out.println("文件名 "+file.getOriginalFilename());
+        //获取文件名
+        String filename = file.getOriginalFilename();
+        //获取后缀
+        filename.substring(filename.lastIndexOf("."));
+        //文件上传的路径
+        String filePath="d:/upload/";
+        //文件名处理
+         filename = filePath + UUID.randomUUID() + filename;
+         //文件对象
+        File dest = new File(filename);
+        //创建路径
+        if (!dest.getParentFile().exists()){
+            dest.getParentFile().mkdir();
+        }
+        try {
+            file.transferTo(dest);
+            return "上传成功";
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
+
+
 
 
 }
